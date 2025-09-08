e17f95fdb2eed5e09770853c79df8fa4fc33d51c
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-08 21:53:34 +0530

    feat(auth): add signup and login pages with landing page setup

--- frontend/resume-builder/src/App.jsx
+++ frontend/resume-builder/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Toaster} from "react-hot-toast";
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Home/Dashboard';
+import EditResume from './pages/ResumeUpdate/EditResume';
 
 const App = () => {
   return (
+    <>
     <div>
-      app
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path='/' element={<LandingPage />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/resume/:resumeId' element={<EditResume />}/>
+        </Routes>
+      </Router>
     </div>
+
+    <Toaster 
+        toastOptions={(
+          className: "",
+          style: {
+            fontSize: "13px",
+          }
+        )}
+      />
+    </>
   )
 }
 
--- frontend/resume-builder/src/pages/Auth/SignUp.jsx
+++ frontend/resume-builder/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 
-const SignUp = () => {
+const SignUp = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  
+  //Handle SignUp Form Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Please enter full name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if(!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //SignUp API Call
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <div>
-      signup
+    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className='text-lg font-semibold text-black'>Create an Account</h3>
+      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+        Join us today by entering your details below
+      </p>
+
+      <form onSubmit={handleSignUp}>
+
+        <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+
+        <div className='grid grid-cols-1 md:grid-cols-1 gap-2'>
+
+          <Input 
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+              />
+
+          <Input 
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="Vimu@gmail.com"
+              type="text"
+              />
+
+          <Input 
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="Min 8 Characters"
+              type="password"
+              />
+        </div>
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+        <button type='submit' className='btn-primary'>
+          SIGN UP
+        </button>
+
+        <p className='text-[13px] text-slate-800 mt-3'>
+          Already an accout?{" "}
+          <button className='font-medium text-primary underline cursor-pointer' onClick={() => {setCurrentPage("login")}}> Login </button>
+        </p>
+      </form>
     </div>
   )
 }
