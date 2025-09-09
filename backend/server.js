f573dcdf0bf2a2b70600ff37d6af1e0892bcc899
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-09 11:28:23 +0530

    feat(backend): connect backend to MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -14,6 +15,9 @@ app.use(
         allowedHeaders: ["Content-Type", "Authorization"]
     })
 );
+
+//Connect Database
+connectDB();
 
 //Middleware
 app.use(express.json());
