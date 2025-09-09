5802cd9178107303e3fa2c859f8e1fc277e015c3
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-09 12:16:40 +0530

    feat(backend): add JWT authentication

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {registerUser, loginUser, getUserProfile} = require("../controllers/authController");
+const {protect} = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+//Auth Routes
+router.post("/register", registerUser); 
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+
+module.exports = router;
