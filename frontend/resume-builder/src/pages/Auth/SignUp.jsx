64a699300f8824b931e82b3b3297ab74eca781ee
Sahan Vimukthi <svimukthi7777@gmail.com>
2025-09-11 08:47:38 +0530

    feat(frontend): implement login form with backend integration

--- frontend/resume-builder/src/pages/Auth/SignUp.jsx
+++ frontend/resume-builder/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
 import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp = ({ setCurrentPage }) => {
   const [profilePic, setProfilePic] = useState(null);
@@ -12,6 +16,7 @@ const SignUp = ({ setCurrentPage }) => {
 
   const [error, setError] = useState(null);
 
+  const {updateUser} = useContext(UserContext);
   const navigate = useNavigate();
   
   //Handle SignUp Form Submit
@@ -39,9 +44,32 @@ const SignUp = ({ setCurrentPage }) => {
 
     //SignUp API Call
     try {
-      
+      //Upload image
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const { token } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
     } catch (error) {
-      
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.")
+      }
     }
   }
 
---
+++ frontend/resume-builder/src/utils/uploadImage.js
@@ -0,0 +1,22 @@
+import { API_PATHS } from "./apiPaths";
+import axiosInstance from "./axiosInstance";
+
+const uploadImage = async (imageFile) => {
+    const formData = new FormData();
+    //Append image file to form data
+    formData.append('image', imageFile);
+
+    try {
+        const response = await axiosInstance.post(API_PATHS.IMAGE.UPLOAD_IMAGES, formData, {
+            headers: {
+                'Content-Type' : 'multipart/form-data', //Set header for file upload
+            },
+        });
+        return response.data; //return response data
+    } catch (error) {
+        console.error('Error uploading the image:', error);
+        throw error; //Rethrow error for handling
+    }
+};
+
+export default uploadImage;
